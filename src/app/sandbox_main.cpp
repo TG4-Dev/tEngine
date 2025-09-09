2a219bd00200f6215f87e5dbdb8f07bdfb747b20
Константин Даль <djkostjan@MacBook-Pro-Konstantin.local>
2025-09-09 17:04:47 +0300

    Added variable length arguments to logger

--- src/app/sandbox_main.cpp
+++ src/app/sandbox_main.cpp
@@ -3,8 +3,16 @@
 
 int main() {
   core::Log::Init();
-  core::Log::GetCoreLogger()->warn("Init log");
-  core::Log::GetClientLogger()->warn("Init log");
+  TE_CORE_TRACE("trace");
+  TE_CORE_INFO("info");
+  TE_CORE_WARN("warn");
+  TE_CORE_ERROR("error");
+  TE_CORE_CRITICAL("critical");
 
+  TE_TRACE("trace");
+  TE_INFO("info");
+  TE_WARN("warn");
+  TE_ERROR("error");
+  TE_CRITICAL("critical");
   return 0;
 }
--- src/core/Log.cpp
+++ src/core/Log.cpp
@@ -2,6 +2,7 @@
 #include "spdlog/common.h"
 #include "spdlog/logger.h"
 #include "spdlog/sinks/stdout_color_sinks.h"
+#include "spdlog/sinks/basic_file_sink.h"
 #include "spdlog/spdlog.h"
 
 namespace core {
@@ -10,11 +11,21 @@ std::shared_ptr<spdlog::logger> Log::s_CoreLogger;
 std::shared_ptr<spdlog::logger> Log::s_ClientLogger;
 
 void core::Log::Init() {
-  spdlog::set_pattern("%^[%T] %n: %v%$");
-  s_CoreLogger = spdlog::stdout_color_mt("tEngine");
-  s_CoreLogger->set_level(spdlog::level::trace);
+  std::vector<spdlog::sink_ptr> logSinks;
+  logSinks.emplace_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
+  logSinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("tEngine.log", true));
 
-  s_ClientLogger = spdlog::stdout_color_mt("APP");
+
+  logSinks[0]->set_pattern("%^[%T] %n: %v%$");
+  logSinks[1]->set_pattern("[%T] [%l] %n: %v");
+
+  s_CoreLogger = std::make_shared<spdlog::logger>("tEngine", begin(logSinks), end(logSinks));
+  spdlog::register_logger(s_CoreLogger);
+  s_CoreLogger->set_level(spdlog::level::trace);
+  s_CoreLogger->flush_on(spdlog::level::trace);
+
+  s_ClientLogger = std::make_shared<spdlog::logger>("APP", begin(logSinks), end(logSinks));
+  spdlog::register_logger(s_ClientLogger);
   s_ClientLogger->set_level(spdlog::level::trace);
-}
+  s_ClientLogger->flush_on(spdlog::level::trace);}
 } // namespace core
