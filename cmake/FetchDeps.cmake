b5de5ea02e89fa0c1c243c42d437a0f409895300
Константин Даль <djkostjan@MacBook-Pro-Konstantin.local>
2025-09-10 00:11:33 +0300

    fixed program doesnt compile because of incompatability of fmt and spdlog

--- cmake/FetchDeps.cmake
+++ cmake/FetchDeps.cmake
@@ -4,7 +4,7 @@ include(FetchContent)
 FetchContent_Declare(
   fmt
   GIT_REPOSITORY  https://github.com/fmtlib/fmt.git
-  GIT_TAG         master 
+  GIT_TAG         10.2.1 
 )
 
 FetchContent_MakeAvailable(fmt)
--- src/core/time/Clock.cpp
+++ src/core/time/Clock.cpp
@@ -1,4 +1,3 @@
-#pragma once
 #include "Clock.hpp"
 
 namespace core::time {
